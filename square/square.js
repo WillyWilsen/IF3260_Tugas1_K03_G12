4a093eae4e5875f34f49f49714a81322b3118df1
TubesForLyfe <13520160@std.stei.itb.ac.id>
2023-02-03 19:52:12 +0700

    Color a corner point & color all corner point

--- square/square.js
+++ square/square.js
@@ -18,8 +18,17 @@ var vertex_idx;
 window.onload = function init() {
     const canvas = document.getElementById('square-canvas');
     const save_btn = document.getElementById('save-btn');
+    const rotate = document.getElementById('rotate');
+    const move_corner = document.getElementById('move-corner');
+    const color_corner = document.getElementById('color-corner');
+    const rgb_form = document.getElementById('rgb-form');
+    const r = document.getElementById('r');
+    const g = document.getElementById('g');
+    const b = document.getElementById('b');
+    const rgb_all = document.getElementById('rgb-all');
+    const radioButtons = document.querySelectorAll('input[name="choice"]')
+    
     const gl = canvas.getContext('webgl');
-
     if (!gl) {
         throw new Error('WebGL not supported');
     }
@@ -70,49 +79,89 @@ window.onload = function init() {
     // render
     render(gl, program);
 
-    // interval for rotate
-    interval = setInterval(function() {
-        vertexData = rotate(vertexData, 1);
-        // render
-        render(gl, program);
-    }, 10);
-
-    canvas.onmouseenter = function() {
-        clearInterval(interval);
-    };
-
-    canvas.onmouseleave = function() {
+    // ROTATE
+    rotate.onclick = function() {
+        r.value = '';
+        g.value = '';
+        b.value = '';
+        rgb_form.hidden = true;
         interval = setInterval(function() {
-            vertexData = rotate(vertexData, 1);
+            vertexData = rotateVertex(vertexData, 1);
             // render
             render(gl, program);
         }, 10);
     }
 
-    // slider for corner point
+    move_corner.onclick = function() {
+        clearInterval(interval);
+        r.value = '';
+        g.value = '';
+        b.value = '';
+        rgb_form.hidden = true;
+    }
+
+    color_corner.onclick = function() {
+        clearInterval(interval);
+        rgb_form.hidden = false;
+    }
+
     canvas.onmousedown = function(e) {
-        x = ((e.clientX - e.target.offsetLeft - canvas.width / 2) / (canvas.width / 2)).toFixed(2);
-        y = ((-1) * (e.clientY - e.target.offsetTop - canvas.height / 2) / (canvas.height / 2)).toFixed(2);
-        for (let i = 0; i < vertexData.length; i += 2) {
-            if ((x >= vertexData[i].toFixed(2) - 0.05 && x <= vertexData[i].toFixed(2) + 0.05) 
-            && (y >= vertexData[i + 1].toFixed(2) - 0.05 && y <= vertexData[i + 1].toFixed(2) + 0.05)) {
-                vertex_idx = i;
+        // MOVE CORNER POINT (MOUSE DOWN)
+        if (getChoice(radioButtons) === "move-corner") {
+            x = ((e.clientX - e.target.offsetLeft - canvas.width / 2) / (canvas.width / 2)).toFixed(2);
+            y = ((-1) * (e.clientY - e.target.offsetTop - canvas.height / 2) / (canvas.height / 2)).toFixed(2);
+            for (let i = 0; i < vertexData.length; i += 2) {
+                if ((x >= vertexData[i].toFixed(2) - 0.05 && x <= vertexData[i].toFixed(2) + 0.05) 
+                && (y >= vertexData[i + 1].toFixed(2) - 0.05 && y <= vertexData[i + 1].toFixed(2) + 0.05)) {
+                    vertex_idx = i;
+                }
+            }
+        // COLOR CORNER POINT
+        } else if (getChoice(radioButtons) == "color-corner") {
+            if (isValidRGB(r, g, b)) {
+                x = ((e.clientX - e.target.offsetLeft - canvas.width / 2) / (canvas.width / 2)).toFixed(2);
+                y = ((-1) * (e.clientY - e.target.offsetTop - canvas.height / 2) / (canvas.height / 2)).toFixed(2);
+                for (let i = 0; i < vertexData.length; i += 2) {
+                    if ((x >= vertexData[i].toFixed(2) - 0.05 && x <= vertexData[i].toFixed(2) + 0.05) 
+                    && (y >= vertexData[i + 1].toFixed(2) - 0.05 && y <= vertexData[i + 1].toFixed(2) + 0.05)) {
+                        colors[i / 2 * 3] = r.value / 255;
+                        colors[i / 2 * 3 + 1] = g.value / 255;
+                        colors[i / 2 * 3 + 2] = b.value / 255;
+                        // render
+                        render(gl, program);
+                    }
+                }   
             }
         }
     }
 
     canvas.onmouseup = function(e) {
-        if (vertex_idx != null) {
+        // MOVE CORNER POINT (MOUSE UP)
+        if (getChoice(radioButtons) === "move-corner" && vertex_idx != null) {
             x = (e.clientX - e.target.offsetLeft - canvas.width / 2) / (canvas.width / 2);
             y = (-1) * (e.clientY - e.target.offsetTop - canvas.height / 2) / (canvas.height / 2);
             vertexData[vertex_idx] = x;
             vertexData[vertex_idx + 1] = y;
+            // render
             render(gl, program);
             vertex_idx = null;
         }
     }
 
-    // save file
+    // COLOR ALL CORNER POINT
+    rgb_all.onclick = function() {
+        if (isValidRGB(r, g, b)) {
+            for (let i = 0; i < colors.length; i += 3) {
+                colors[i] = r.value / 255;
+                colors[i + 1] = g.value / 255;
+                colors[i + 2] = b.value / 255;
+                // render
+                render(gl, program);
+            }
+        }
+    }
+
+    // SAVE FILE
     save_btn.onclick = function() {
         var vertexString = '';
         for (let i = 0; i < vertexData.length; i += 2) {
@@ -125,17 +174,26 @@ window.onload = function init() {
     }
 }
 
-function rotate(vertexData, angle) {
+function getChoice(radioButtons) {
+    for (const radioButton of radioButtons) {
+        if (radioButton.checked) {
+            return radioButton.value;
+        }
+    }
+    return null;
+}
+
+function isValidRGB(r, g, b) {
+    return r.value >= 0 && r.value <= 255 && g.value >= 0 && g.value <= 255 && b.value >= 0 && b.value <= 255;
+}
+
+function rotateVertex(vertexData, angle) {
     const radians = (angle * Math.PI) / 180.0;
     const newVertex = [];
-    newVertex.push(Math.cos(radians) * vertexData[0] - Math.sin(radians) * vertexData[1]);
-    newVertex.push(Math.sin(radians) * vertexData[0] + Math.cos(radians) * vertexData[1]);
-    newVertex.push(Math.cos(radians) * vertexData[2] - Math.sin(radians) * vertexData[3]);
-    newVertex.push(Math.sin(radians) * vertexData[2] + Math.cos(radians) * vertexData[3]);
-    newVertex.push(Math.cos(radians) * vertexData[4] - Math.sin(radians) * vertexData[5]);
-    newVertex.push(Math.sin(radians) * vertexData[4] + Math.cos(radians) * vertexData[5]);
-    newVertex.push(Math.cos(radians) * vertexData[6] - Math.sin(radians) * vertexData[7]);
-    newVertex.push(Math.sin(radians) * vertexData[6] + Math.cos(radians) * vertexData[7]);
+    for (let i = 0; i < vertexData.length; i += 2) {
+        newVertex.push(Math.cos(radians) * vertexData[i] - Math.sin(radians) * vertexData[i + 1]);
+        newVertex.push(Math.sin(radians) * vertexData[i] + Math.cos(radians) * vertexData[i + 1]);
+    }
     return newVertex;
 }
 
