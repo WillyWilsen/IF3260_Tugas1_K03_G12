7392a41d3edac244d03403e44c0f3bff611510a0
TubesForLyfe <13520160@std.stei.itb.ac.id>
2023-02-03 15:10:21 +0700

    Add custom color to buffer

--- square/square.js
+++ square/square.js
@@ -6,6 +6,12 @@ var vertexData = [
     -0.5, -0.5,
     -0.5, 0.5
 ];
+var colors = [
+    1, 1, 1,
+    1, 1, 1,
+    1, 1, 1,
+    1, 1, 1
+]
 var interval;
 var vertex_idx;
 
@@ -18,28 +24,36 @@ window.onload = function init() {
         throw new Error('WebGL not supported');
     }
 
-    // create buffer
-    const buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-
     // create vertex shader
     const vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader, `
-    attribute vec3 position;
+    precision mediump float;
+    attribute vec2 position;
+    attribute vec3 color;
+    varying vec3 vColor;
     void main() {
-        gl_Position = vec4(position, 1);
+        gl_Position = vec4(position, 0, 1);
+        vColor = color;
     }
     `);
     gl.compileShader(vertexShader);
+    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)){
+        console.error(gl.getShaderInfoLog(vertexShader));
+    }
 
     // create fragment shader
     const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShader, `
+    precision mediump float;
+    varying vec3 vColor;
     void main() {
-        gl_FragColor = vec4(1, 1, 1, 1);
+        gl_FragColor = vec4(vColor, 1);
     }
     `);
     gl.compileShader(fragmentShader);
+    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)){
+        console.error(gl.getShaderInfoLog(fragmentShader));
+    }
 
     // create program
     const program = gl.createProgram();
@@ -48,21 +62,19 @@ window.onload = function init() {
     gl.attachShader(program, vertexShader);
     gl.attachShader(program, fragmentShader);
     gl.linkProgram(program);
-
-    // enable vertex attributes
-    const positionLocation = gl.getAttribLocation(program, `position`);
-    gl.enableVertexAttribArray(positionLocation);
-    gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)){
+        console.error(gl.getProgramInfoLog(program));
+    }
     gl.useProgram(program);
 
     // render
-    render(gl, vertexData);
+    render(gl, program);
 
     // interval for rotate
     interval = setInterval(function() {
         vertexData = rotate(vertexData, 1);
         // render
-        render(gl, vertexData);
+        render(gl, program);
     }, 10);
 
     canvas.onmouseenter = function() {
@@ -73,10 +85,9 @@ window.onload = function init() {
         interval = setInterval(function() {
             vertexData = rotate(vertexData, 1);
             // render
-            render(gl, vertexData);
+            render(gl, program);
         }, 10);
     }
-
 
     // slider for corner point
     canvas.onmousedown = function(e) {
@@ -96,7 +107,7 @@ window.onload = function init() {
             y = (-1) * (e.clientY - e.target.offsetTop - canvas.height / 2) / (canvas.height / 2);
             vertexData[vertex_idx] = x;
             vertexData[vertex_idx + 1] = y;
-            render(gl, vertexData);
+            render(gl, program);
             vertex_idx = null;
         }
     }
@@ -128,12 +139,30 @@ function rotate(vertexData, angle) {
     return newVertex;
 }
 
-function render(gl, vertexData) {
-    // clear canvas
-    gl.clear(gl.COLOR_BUFFER_BIT);
+function render(gl, program) {
+    // create vertex buffer
+    const vertex_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
 
     // load vertexData into buffer
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+
+    // enable vertex attributes
+    const position = gl.getAttribLocation(program, `position`);
+    gl.enableVertexAttribArray(position);
+    gl.vertexAttribPointer(position, 2, gl.FLOAT, false, 0, 0);
+
+    // create color buffer
+    const color_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
+
+    // load color into buffer
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+    // enable color attributes
+    const color = gl.getAttribLocation(program, `color`);
+    gl.enableVertexAttribArray(color);
+    gl.vertexAttribPointer(color, 3, gl.FLOAT, false, 0, 0);
 
     // draw
     gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
